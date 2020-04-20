f7baec75de4de3c4e57a4b20bcf5bddd40e9561a
Maciej Krüger <mkg20001@gmail.com>
2020-04-20 07:01:10 +0200

    feat: add test.sh

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Needs device..." >&2
+  exit 2
+fi
+
+export RUST_BACKTRACE=1
+
+exec sudo -E target/debug/distinst \
+    -h "meros-testing" \
+    -k "us" \
+    -l "en_US.UTF-8" \
+    -b "$1" \
+    -t "$1:gpt" \
+    -n "$1:primary:start:512M:fat32:mount=/boot/efi:flags=esp" \
+    -n "$1:primary:512M:-4096M:ext4:mount=/" \
+    -n "$1:primary:-4096M:end:swap" \
+    --username "meros" \
+    --realname "MerOS User" \
+    --tz "Etc/UTC"
