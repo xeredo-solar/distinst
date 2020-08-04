761b3edcc733ba5d7a39af89417fa9023b4f16e2
Maciej Krüger <mkg20001@gmail.com>
2020-08-04 08:29:50 +0200

    feat: use dev.nix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,9 @@
-with (import <nixpkgs> {});
 let
   solaros = (builtins.fetchTarball https://nix.ssd-solar.dev/dev/solaros/nixexprs.tar.xz);
   solarnixpkgs = (builtins.fetchTarball https://nix.ssd-solar.dev/dev/nixpkgs/nixexprs.tar.xz);
 in
+with (import "${solaros}/dev.nix");
 callPackage ./package.nix {
-  conf-tool = ((import solaros).conf-tool);
   shellHookAppend = ''
     export NIX_PATH="solaros=${solaros}:nixpkgs=${solarnixpkgs}"
   '';
